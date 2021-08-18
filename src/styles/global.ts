4728da1729ee813a552b0fba1d6c1aa40ea3d16e
leoarmelin <leonardoarmelin@codeby.com.br>
2021-08-17 21:54:48 -0300

    feat(global): create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors.grayDark};
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  body, input, textarea, select, button {
+    font: 400 1rem "Roboto", sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
