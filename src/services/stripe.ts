cf82c5b19be4210643244973a631be2479d933ec
leoarmelin <leonardo99.armelin@gmail.com>
2021-10-20 19:45:54 -0300

    chore(stripe): update service

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,9 @@
-import Stripe from 'stripe';
-import { version } from '../../package.json';
+import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
-  apiVersion: '2020-08-27',
+  apiVersion: "2020-08-27",
   appInfo: {
-    name: 'ignews-leonardo',
-    version,
+    name: "ignews-leonardo",
+    version: "0.1.0",
   },
 });
