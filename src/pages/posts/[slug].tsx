edd1796d8acaf9738c473b32bae92189a397b2d8
leoarmelin <leonardo99.armelin@gmail.com>
2021-10-23 14:46:11 -0300

    feat(Posts): create page

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,80 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
+import { RichText } from "prismic-dom";
+import { SEO } from "../../components/SEO";
+import { getPrismicClient } from "../../services/prismic";
+
+import {
+  Container,
+  PostWrapper,
+  Time,
+  Title,
+  Content,
+} from "../../styles/Post";
+
+interface IPostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+export default function Post({ post }: IPostProps) {
+  return (
+    <>
+      <SEO title={post.title} />
+
+      <Container>
+        <PostWrapper>
+          <Title>{post.title}</Title>
+          <Time>{post.updatedAt}</Time>
+
+          <Content dangerouslySetInnerHTML={{ __html: post.content }} />
+        </PostWrapper>
+      </Container>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
+  const { slug } = params;
+
+  if (!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const prismic = getPrismicClient(req);
+
+  const response = await prismic.getByUID("post", String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
